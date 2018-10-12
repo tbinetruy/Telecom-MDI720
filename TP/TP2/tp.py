fddc301424ae32262f0f0219fbe4e91d78c0e812
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-10-12 09:02:09 +0200

    refactred

--- TP/TP2/tp.py
+++ TP/TP2/tp.py
@@ -2,22 +2,33 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 
 def import_data():
     df = pd.read_csv('invest.txt', delimiter=" ")
     return df
 
+def log_scale(vect):
+    return np.log(vect)
+
+def normalize_df(data):
+    data["gnp"] = log_scale(data["gnp"])
+    data["invest"] = log_scale(data["invest"])
 
 def plot_gnp_vs_invest(data):
+    gnp = data["gnp"]
+    invest = data["invest"]
     plt.figure()
-    plt.plot(data["gnp"], data["invest"], color='blue', label='Father')
+    plt.plot(gnp, invest, color='blue', label='Father')
     plt.ylabel('Invest', fontsize=18)
     plt.xlabel('GNP', fontsize=18)
     plt.title("GNP vs Invest")
     plt.show()
 
+def main():
+    df = import_data()
+    normalize_df(df)
+    plot_gnp_vs_invest(df)
 
-df = import_data()
-print(df)
-plot_gnp_vs_invest(df)
+main()
